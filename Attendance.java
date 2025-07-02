aa1f5b454c10468a20d94cb45a5942bbe73b8c72
23AD159 <153845833+23AD159@users.noreply.github.com>
2025-07-02 18:51:49 +0530

    Attendance.java
    Attendance class

---
+++ Attendance.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Attendance {
+    public void studentslist(){
+        System.out.println("List of students present in code studio");
+    }
+    public void studentslist2(int rollno){
+        System.out.println("No of Absentees: "+rollno);
+    }
+}
