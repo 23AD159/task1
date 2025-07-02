6b9687fbd6e8eab4947d25f6c7290850a45a7945
23AD159 <153845833+23AD159@users.noreply.github.com>
2025-07-02 18:50:32 +0530

    Create Assessment.java
    Assessment class

---
+++ Assessment.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Assessment {
+    public void task(){
+        System.out.println("Daily task has been assigned to students");
+    }
+    public void task2(String s){
+        System.out.println("kudos for assessment completion");
+    }
+}
